9db3f946c21f140ab70aeb474025ed495c929ef7
Aman Singh <amans@advantageclub.ai>
2025-09-24 12:36:43 +0530

    Setup: Added clerk package for managing user authentication

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,14 +1,26 @@
 import { Stack } from "expo-router";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import { ClerkLoaded, ClerkProvider } from '@clerk/clerk-expo'
+import { tokenCache } from '@clerk/clerk-expo/token-cache'
 
+const publishedKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!
+
+if(!publishedKey){
+  throw new Error(
+    "Missing Published Key."
+  );
+}
 export default function RootLayout() {
   return (
-    <SafeAreaProvider>
-        <SafeAreaView style = {{flex: 1}}>
-            <Stack screenOptions={{headerShown: false}} >
-                
-            </Stack>
-        </SafeAreaView>
-    </SafeAreaProvider>
+    <ClerkProvider publishableKey={publishedKey} tokenCache={tokenCache}>
+      <ClerkLoaded>
+        <SafeAreaProvider>
+            <SafeAreaView style = {{flex: 1, backgroundColor: "#000"}}>
+                <Stack screenOptions={{headerShown: false}} >
+                </Stack>
+            </SafeAreaView>
+        </SafeAreaProvider>
+      </ClerkLoaded>
+    </ClerkProvider>
   );
 }
